7e39388b612f3fb12d97650b032b514680ef6e17
Chris Doyle <archslide@gmail.com>
2012-04-21 14:21:33 -0500

    spec for constructive_merge in place

--- spec/hash_extensions_spec.rb
+++ spec/hash_extensions_spec.rb
@@ -9,3 +9,13 @@ describe "#constructive_merge" do
     x.constructive_merge(y).should == {'reviews' => {'user' => {}, 'ratings' => {}}}
   end
 end
+
+describe "#constructive_merge!" do
+  it "merges hashes in place without clobbering" do
+    x = {'reviews' => {'user' => {}}}
+    y = {'reviews' => {'ratings' => {}}}
+    x.extend TablePrint::HashExtensions::ConstructiveMerge
+    x.constructive_merge!(y)
+    x.should == {'reviews' => {'user' => {}, 'ratings' => {}}}
+  end
+end
