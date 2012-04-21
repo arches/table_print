db4e7b3b233c64c335ced8aa538d1e55f820ef79
Chris Doyle <archslide@gmail.com>
2012-04-21 14:34:55 -0500

    remove unused files

--- lib/cell.rb
+++
@@ -1,7 +0,0 @@
-module TablePrint
-  class Cell
-    attr_accessor :row, :column, :wrapped_object
-
-    # could be empty (if the column's method doesn't apply to this object type)
-  end
-end
--- lib/column.rb
+++
@@ -1,9 +0,0 @@
-module TablePrint
-
-  # note: if we have columns for "user.blogs.url" and "user.blogs.title", we only want to add one set of user.blogs to the stack
-  class Column
-    # knows its width
-    # holds the method
-    # has a name
-  end
-end
