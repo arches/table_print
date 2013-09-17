8aeff7dca86404bb4a45a104172b2d2788cbe5c7
Andrew Selder <aselder@mac.com>
2013-09-17 09:57:46 -0700

    Force Struct members to symbols.
    Ruby 1.8 structs return members as strings while 1.9 structs return symbols.

--- spec/printable_spec.rb
+++ spec/printable_spec.rb
@@ -46,11 +46,6 @@ describe TablePrint::Printable do
 
     it "uses column information when available (eg, from ActiveRecord objects)"
     
-    it "uses keys when passed a Hash" do
-      hash = {:foo => 1, :bar => 2}
-      TablePrint::Printable.default_display_methods(hash).should == [:foo, :bar]
-    end
-    
     it "uses the members method when passed a Struct" do
       test_struct = Struct.new(:foo, :bar)
       obj = test_struct.new
