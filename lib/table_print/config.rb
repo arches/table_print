103d40050cf966f07b5fea5272e6601a82c9f009
Chris Doyle <chris@arches.io>
2016-08-05 15:47:45 -0500

    refactor config to use singleton instance instead of class for globals

--- lib/table_print/config.rb
+++ lib/table_print/config.rb
@@ -1,6 +1,14 @@
 module TablePrint
   class Config
 
+    attr_accessor :capitalize_headers,
+      :io,
+      :klasses,
+      :max_width,
+      :multibyte,
+      :separator,
+      :time_format
+
     DEFAULT_MAX_WIDTH = 30
     DEFAULT_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
     DEFAULT_IO = $stdout
@@ -16,21 +24,23 @@ module TablePrint
 
     @@klasses = {}
 
+    @@singleton = nil
+
     def self.set(klass, val)
       if klass.is_a? Class
-        @@klasses[klass] = val  # val is a hash of column options
+        singleton.klasses[klass] = val  # val is a hash of column options
       else
         TablePrint::Config.send("#{klass}=", val.first)
       end
     end
 
     def self.for(klass)
-      @@klasses.fetch(klass) {}
+      singleton.klasses.fetch(klass) {}
     end
 
     def self.clear(klass)
       if klass.is_a? Class
-        @@klasses.delete(klass)
+        singleton.klasses.delete(klass)
       else
         original_value = TablePrint::Config.const_get("DEFAULT_#{klass.to_s.upcase}")
         TablePrint::Config.send("#{klass}=", original_value)
@@ -38,52 +48,74 @@ module TablePrint
     end
 
     def self.max_width
-      @@max_width
+      singleton.max_width
     end
 
     def self.max_width=(width)
-      @@max_width = width
+      singleton.max_width = width
     end
 
     def self.multibyte
-      @@multibyte
+      singleton.multibyte
     end
 
     def self.multibyte=(width)
-      @@multibyte = width
+      singleton.multibyte = width
     end
 
     def self.time_format
-      @@time_format
+      singleton.time_format
     end
 
     def self.time_format=(format)
-      @@time_format = format
+      singleton.time_format = format
     end
 
     def self.capitalize_headers
-      @@capitalize_headers
+      singleton.capitalize_headers
     end
 
     def self.capitalize_headers=(caps)
-      @@capitalize_headers = caps
+      singleton.capitalize_headers = caps
     end
 
     def self.separator
-      @@separator
+      singleton.separator
     end
 
     def self.separator=(separator)
-      @@separator = separator
+      singleton.separator = separator
     end
 
     def self.io
-      @@io
+      singleton.io
     end
 
     def self.io=(io)
       raise StandardError.new("IO object must respond to :puts") unless io.respond_to? :puts
-      @@io = io
+      singleton.io = io
+    end
+
+    def initialize(opts={})
+      self.klasses = {}
+
+      opts.each do |k,v|
+        send("#{k}=", v)
+      end
+
+      self
+    end
+
+    private
+    def self.singleton
+      @@singleton ||= Config.new({
+        capitalize_headers: true,
+        io: DEFAULT_IO,
+        max_width: DEFAULT_MAX_WIDTH,
+        multibyte: false,
+        separator: DEFAULT_SEPARATOR,
+        time_format: DEFAULT_TIME_FORMAT,
+      })
     end
   end
 end
