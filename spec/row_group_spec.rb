b98e8d6771be3e58a7f71074bc91453dc971d3b8
Chris Doyle <chris@arches.io>
2016-09-25 10:33:17 -0500

    uncomment specs

--- spec/row_group_spec.rb
+++ spec/row_group_spec.rb
@@ -24,50 +24,50 @@ describe TablePrint::Row do
     group.add_child(row)
   }
 
-#  describe "#format" do
-#    it "formats the row with padding" do
-#      compare_rows(row.format, "wonky | bob jones | 2012    ")
-#    end
-#
-#    it "also formats the children" do
-#      row.add_child(RowGroup.new.add_child(Row.new.set_cell_values(:title => "wonky2", :author => "bob jones2", :pub_date => "20122")))
-#
-#      table.columns.find{|c| c.name.to_s == "title"}.data << "wonky2"
-#      table.columns.find{|c| c.name.to_s == "author"}.data << "bob jones2"
-#      table.columns.find{|c| c.name.to_s == "pub_date"}.data << "20122"
-#
-#      compare_rows(row.format, ["wonky  | bob jones  | 2012    ", "wonky2 | bob jones2 | 20122   "])
-#    end
-#  end
+  describe "#format" do
+    it "formats the row with padding" do
+      compare_rows(row.format, "wonky | bob jones | 2012    ")
+    end
 
-#  describe "#apply_formatters" do
-#    it "calls the format method on each formatter for that column" do
-#      Sandbox.add_class("DoubleFormatter")
-#      Sandbox.add_method("DoubleFormatter", :format) { |value| value * 2 }
-#
-#      Sandbox.add_class("ChopFormatter")
-#      Sandbox.add_method("ChopFormatter", :format) { |value| value[0..-2] }
-#
-#      f1 = Sandbox::DoubleFormatter.new
-#      f2 = Sandbox::ChopFormatter.new
-#
-#      column = Column.new(:name => "title", :fixed_width => 9)
-#      column.formatters = [f1, f2]
-#
-#      row.apply_formatters(column).should == "wonkywonk"
-#    end
-#
-#    it "uses the config'd time_format to format times" do
-#      column = Column.new(:name => "title", :fixed_width => 20, :formatters => [], :time_format => "%Y %m %d")
-#
-#      time_formatter = TablePrint::TimeFormatter.new
-#      TablePrint::TimeFormatter.should_receive(:new).with("%Y %m %d") {time_formatter}
-#
-#      row.set_cell_values(:title => Time.local(2012, 6, 1, 14, 20, 20))
-#
-#      row.apply_formatters(column)
-#    end
-#  end
+    it "also formats the children" do
+      row.add_child(RowGroup.new.add_child(Row.new.set_cell_values(:title => "wonky2", :author => "bob jones2", :pub_date => "20122")))
+
+      table.columns.find{|c| c.name.to_s == "title"}.data << "wonky2"
+      table.columns.find{|c| c.name.to_s == "author"}.data << "bob jones2"
+      table.columns.find{|c| c.name.to_s == "pub_date"}.data << "20122"
+
+      compare_rows(row.format, ["wonky  | bob jones  | 2012    ", "wonky2 | bob jones2 | 20122   "])
+    end
+  end
+
+  describe "#apply_formatters" do
+    it "calls the format method on each formatter for that column" do
+      Sandbox.add_class("DoubleFormatter")
+      Sandbox.add_method("DoubleFormatter", :format) { |value| value * 2 }
+
+      Sandbox.add_class("ChopFormatter")
+      Sandbox.add_method("ChopFormatter", :format) { |value| value[0..-2] }
+
+      f1 = Sandbox::DoubleFormatter.new
+      f2 = Sandbox::ChopFormatter.new
+
+      column = Column.new(:name => "title", :fixed_width => 9)
+      column.formatters = [f1, f2]
+
+      row.apply_formatters(column).should == "wonkywonk"
+    end
+
+    it "uses the config'd time_format to format times" do
+      column = Column.new(:name => "title", :fixed_width => 20, :formatters => [], :time_format => "%Y %m %d")
+
+      time_formatter = TablePrint::TimeFormatter.new
+      TablePrint::TimeFormatter.should_receive(:new).with("%Y %m %d") {time_formatter}
+
+      row.set_cell_values(:title => Time.local(2012, 6, 1, 14, 20, 20))
+
+      row.apply_formatters(column)
+    end
+  end
 end
 
 
--- spec/row_recursion_spec.rb
+++ spec/row_recursion_spec.rb
@@ -34,80 +34,80 @@ describe RowRecursion do
   end
 end
 
-#describe RowRecursion do
-#  let(:parent) { RowGroup.new }
-#  let(:child) { Row.new }
-#
-#  describe "tabling" do
-#    let(:table) { Table.new }
-#    let(:column) { Column.new(:name => "title", :data => [""]) }
-#    let(:group) { RowGroup.new }
-#
-#    before {
-#      table.add_column(column)
-#      table.add_child(group)
-#      group.add_child(child)
-#    }
-#
-#    describe "#columns" do
-#      it "gets the columns from the root node" do
-#        table.columns.length.should == 1
-#        child.columns.should == table.columns
-#      end
-#    end
-#
-#    describe "#width" do
-#      it "returns the total width of the columns" do
-#
-#        table = Table.new
-#        table.columns = [
-#          Column.new(:name => "title", :data => ["foobar"]),
-#          Column.new(:name => "subtitle", :data => ["elemental"]),
-#        ]
-#
-#        table.width.should == 18
-#      end
-#    end
-#
-#    describe "#horizontal_separator" do
-#      it "returns hyphens equal to the table width" do
-#        pending "move to formatter spec"
-#        return
-#
-#        table = Table.new
-#        table.columns = [
-#          Column.new(:name => "title", :data => %w{ aaaaa aaaaaa }),
-#          Column.new(:name => "description", :data => %w{ bbb bbbb }),
-#          Column.new(:name => "category", :data => %w{ cccccccccc ccccccccc}),
-#        ]
-#
-#        table.header.size.should == table.horizontal_separator.size
-#        compare_rows(table.horizontal_separator, '-' * 6 + '-|-' + '-' * 'description'.size + '-|-' + '-' * 10)
-#      end
-#
-#      it "matches the header width" do
-#        pending "move to formatter spec"
-#        return
-#
-#        column.data = %w{foobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobar}
-#        table.horizontal_separator.should == '------------------------------' # 30 hyphens
-#      end
-#    end
-#
-#    describe "#header" do
-#      it "returns the column names, padded to the proper width, separated by the | character" do
-#        pending "move to formatter spec"
-#        return
-#
-#        table.columns = [
-#          Column.new(:name => "title", :data => ['first post']),
-#          Column.new(:name => "author", :data => ['chris']),
-#          Column.new(:name => "subtitle", :data => ['first is the worst']),
-#        ]
-#
-#        compare_rows(table.header, "AUTHOR | SUBTITLE           | TITLE     ")
-#      end
-#    end
-#  end
-#end
+describe RowRecursion do
+  let(:parent) { RowGroup.new }
+  let(:child) { Row.new }
+
+  describe "tabling" do
+    let(:table) { Table.new }
+    let(:column) { Column.new(:name => "title", :data => [""]) }
+    let(:group) { RowGroup.new }
+
+    before {
+      table.add_column(column)
+      table.add_child(group)
+      group.add_child(child)
+    }
+
+    describe "#columns" do
+      it "gets the columns from the root node" do
+        table.columns.length.should == 1
+        child.columns.should == table.columns
+      end
+    end
+
+    describe "#width" do
+      it "returns the total width of the columns" do
+
+        table = Table.new
+        table.columns = [
+          Column.new(:name => "title", :data => ["foobar"]),
+          Column.new(:name => "subtitle", :data => ["elemental"]),
+        ]
+
+        table.width.should == 18
+      end
+    end
+
+    describe "#horizontal_separator" do
+      it "returns hyphens equal to the table width" do
+        pending "move to formatter spec"
+        return
+
+        table = Table.new
+        table.columns = [
+          Column.new(:name => "title", :data => %w{ aaaaa aaaaaa }),
+          Column.new(:name => "description", :data => %w{ bbb bbbb }),
+          Column.new(:name => "category", :data => %w{ cccccccccc ccccccccc}),
+        ]
+
+        table.header.size.should == table.horizontal_separator.size
+        compare_rows(table.horizontal_separator, '-' * 6 + '-|-' + '-' * 'description'.size + '-|-' + '-' * 10)
+      end
+
+      it "matches the header width" do
+        pending "move to formatter spec"
+        return
+
+        column.data = %w{foobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobar}
+        table.horizontal_separator.should == '------------------------------' # 30 hyphens
+      end
+    end
+
+    describe "#header" do
+      it "returns the column names, padded to the proper width, separated by the | character" do
+        pending "move to formatter spec"
+        return
+
+        table.columns = [
+          Column.new(:name => "title", :data => ['first post']),
+          Column.new(:name => "author", :data => ['chris']),
+          Column.new(:name => "subtitle", :data => ['first is the worst']),
+        ]
+
+        compare_rows(table.header, "AUTHOR | SUBTITLE           | TITLE     ")
+      end
+    end
+  end
+end
 
